81fc8c85b7eece2b4996cebaa9225b6e12d7cc2e
LeandroBecker <leandro.becker@ufsc.br>
2023-06-07 14:25:30 +0100

    now embedded-mas works properly with CB2A

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # critical-embedded-mas
 Version of embedded-mas addapted for Jason(C)
+
+To work properly, it is necessary to create a new class that extends DefaultEmbeddedAgArch like, for example, DemoEmbeddedAgentArch. In such new class it is necessary to override the method perceiveCBS() with the given code. Moreover, currently, one must also declare the following variable:
+	private int lastCPval = -12345;  
+
+An example of such class, ready to be used, is given in git-URL.
+
+Another thing is to edit the YAML file with the ROS topics and action, selecting one of the topic being listened to be the critical event. Such topic should have 'beliefName: critical_percept'. 
--- src/main/java/embedded/mas/bridges/jacamo/DefaultEmbeddedAgArch.java
+++ src/main/java/embedded/mas/bridges/jacamo/DefaultEmbeddedAgArch.java
@@ -15,41 +15,41 @@ public class DefaultEmbeddedAgArch extends AgArch{
 
 	protected Collection <DefaultDevice> devices = null;
 
-	private int lastCPval = -12345; //LBB: this is temporary, requires a better implementation
+	//private int lastCPval = -12345; //LBB: this is temporary, requires a better implementation
 
 	public DefaultEmbeddedAgArch() {		
 		super();
 
 	}
 
-	@Override
-	public Boolean[] perceiveCBS() { //v0: Still not generic, works only for the hand-coded perception "value2"
-		Boolean[] percepts = new Boolean[8];
-		for(int i=0; i<8; i++)
-			percepts[i] = Boolean.FALSE;
+	// @Override
+	// public Boolean[] perceiveCBS() { //v0: Still not generic, works only for the hand-coded perception "value2"
+	// 	Boolean[] percepts = new Boolean[8];
+	// 	for(int i=0; i<8; i++)
+	// 		percepts[i] = Boolean.FALSE;
 		
-		for(IDevice s:this.devices) { //for each sensor
-			try {
-				Collection<Literal> p = s.getPercepts(); //List<Literal> beliefs = microcontroller.read(); return beliefs;
-				if(p!=null) {
-					for(Literal l:p) {
-						String functor = l.getFunctor();
-						if(functor.contains("detected_failure")){
-							int value = Integer.parseInt(l.getTerm(0).toString());
-							if(value != lastCPval){
-								lastCPval = value;
-								percepts[0] = Boolean.TRUE;
-							}
-							// else
-							// 	percepts[0] = Boolean.FALSE;
-						}
+	// 	for(IDevice s:this.devices) { //for each sensor
+	// 		try {
+	// 			Collection<Literal> p = s.getPercepts(); //List<Literal> beliefs = microcontroller.read(); return beliefs;
+	// 			if(p!=null) {
+	// 				for(Literal l:p) {
+	// 					String functor = l.getFunctor();
+	// 					if(functor.contains("critical_percept")){
+	// 						int value = Integer.parseInt(l.getTerm(0).toString());
+	// 						if(value != lastCPval){
+	// 							lastCPval = value;
+	// 							percepts[0] = Boolean.TRUE;
+	// 						}
+	// 						// else
+	// 						// 	percepts[0] = Boolean.FALSE;
+	// 					}
 							
-					}
-				}
-			} catch (PerceivingException e) {} //if it fails, do nothing 			
-		}
-		return percepts;
-   	}
+	// 				}
+	// 			}
+	// 		} catch (PerceivingException e) {} //if it fails, do nothing 			
+	// 	}
+	// 	return percepts;
+   	// }
   
 	@Override
 	public void stop() {
